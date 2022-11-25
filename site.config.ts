0193e075254a60c70f3f25ff91a5574128f46ed8
breezjin <breezjin@gmail.com>
2022-11-26 06:44:00 +0900

    add: instagram link

--- site.config.ts
+++ site.config.ts
@@ -22,6 +22,7 @@ export default siteConfig({
   twitter: 'dreamer05',
   github: 'breezjin',
   linkedin: 'yoonjinho',
+  instagram: 'breezjin',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
