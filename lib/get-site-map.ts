3586526733c20de402715708cfb6147243d9c618
breezjin <breezjin@gmail.com>
2022-11-28 15:32:34 +0900

    fix: code order

--- lib/get-site-map.ts
+++ lib/get-site-map.ts
@@ -10,7 +10,12 @@ import { notion } from './notion-api'
 
 const uuid = !!includeNotionIdInUrls
 /** 캐시 타이밍 체크 - 이건 사이트맵 */
-const cache = new ExpiryMap(20000)
+const cache = new ExpiryMap(10000)
+
+const getAllPages = pMemoize(getAllPagesImpl, {
+  cacheKey: (...args) => JSON.stringify(args),
+  cache
+})
 
 export async function getSiteMap(): Promise<types.SiteMap> {
   const partialSiteMap = await getAllPages(
@@ -23,11 +28,6 @@ export async function getSiteMap(): Promise<types.SiteMap> {
     ...partialSiteMap
   } as types.SiteMap
 }
-
-const getAllPages = pMemoize(getAllPagesImpl, {
-  cacheKey: (...args) => JSON.stringify(args),
-  cache
-})
 
 async function getAllPagesImpl(
   rootNotionPageId: string,
