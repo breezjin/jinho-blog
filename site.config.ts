aa0d518fc5497e1f1dcab7b5d0d91cc75fae7cf2
breezjin <breezjin@gmail.com>
2022-12-01 12:42:16 +0900

    fix: root id

--- site.config.ts
+++ site.config.ts
@@ -3,7 +3,7 @@ import { siteConfig } from './lib/site-config'
 export default siteConfig({
   // the site's root Notion page (required)
   // rootNotionPageId: '7875426197cf461698809def95960ebf',
-  rootNotionPageId: 'Jinho-Blog-66f1ee3f56a44e46876f5c9a357db60f',
+  rootNotionPageId: '66f1ee3f56a44e46876f5c9a357db60f',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
